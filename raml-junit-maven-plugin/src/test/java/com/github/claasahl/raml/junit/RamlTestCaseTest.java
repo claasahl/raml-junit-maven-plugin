43e8d0c26f8b1ed37d1b7e2af533769e76106287
Claas Ahlrichs <claasahl@gmail.com>
2017-02-22 07:11:20 +0100

    created tests for equals and hashCode functions

---
+++ raml-junit-maven-plugin/src/test/java/com/github/claasahl/raml/junit/RamlTestCaseTest.java
@@ -0,0 +1,60 @@
+package com.github.claasahl.raml.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class RamlTestCaseTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void implementationOfEqualsShouldHandleNull() {
+		RamlTestCase testCase = new RamlTestCase();
+		assertFalse(testCase.equals(null));
+	}
+
+	@Test
+	public void implementationOfEqualsShouldBeReflexive() {
+		RamlTestCase testCase = new RamlTestCase();
+		assertTrue(testCase.equals(testCase));
+	}
+
+	@Test
+	public void implementationOfEqualsShouldBeSymmetric() {
+		RamlTestCase testCaseA = new RamlTestCase();
+		RamlTestCase testCaseB = new RamlTestCase();
+		assertTrue(testCaseA.equals(testCaseB));
+		assertTrue(testCaseB.equals(testCaseA));
+	}
+
+	@Test
+	public void implementationOfEqualsShouldBeTransitive() {
+		RamlTestCase testCaseA = new RamlTestCase();
+		RamlTestCase testCaseB = new RamlTestCase();
+		RamlTestCase testCaseC = new RamlTestCase();
+		assertTrue(testCaseA.equals(testCaseB));
+		assertTrue(testCaseB.equals(testCaseC));
+		assertTrue(testCaseA.equals(testCaseC));
+	}
+	
+	@Test
+	public void implementationOfHashCodeShouldBeReproducible() {
+		RamlTestCase testCase = new RamlTestCase();
+		int hashCode1 = testCase.hashCode();
+		int hashCode2 = testCase.hashCode();
+		assertEquals(hashCode1, hashCode2);
+	}
+	
+	@Test
+	public void implementationOfHashCodeShouldBeConsistent() {
+		RamlTestCase testCaseA = new RamlTestCase();
+		RamlTestCase testCaseB = new RamlTestCase();
+		assertEquals(testCaseA.hashCode(), testCaseB.hashCode());
+	}
+
+}
