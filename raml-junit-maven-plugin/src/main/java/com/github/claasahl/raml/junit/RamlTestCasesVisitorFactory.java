2b4d8ac029fc0416900eec020c09bcae57b4552a
Claas Ahlrichs <claasahl@gmail.com>
2017-03-01 08:30:49 +0100

    optionally initialize factory with existing builder

--- raml-junit-maven-plugin/src/main/java/com/github/claasahl/raml/junit/RamlTestCasesVisitorFactory.java
+++ raml-junit-maven-plugin/src/main/java/com/github/claasahl/raml/junit/RamlTestCasesVisitorFactory.java
@@ -1,5 +1,7 @@
 package com.github.claasahl.raml.junit;
 
+import java.util.function.Supplier;
+
 import com.github.claasahl.raml.visitor.ActionVisitor;
 import com.github.claasahl.raml.visitor.RamlVisitor;
 import com.github.claasahl.raml.visitor.RamlVisitorFactory;
@@ -8,26 +10,41 @@ import com.github.claasahl.raml.visitor.ResponseVisitor;
 
 public class RamlTestCasesVisitorFactory implements RamlVisitorFactory {
 	
-	private final RamlTestCaseBuilder builder = new RamlTestCaseBuilder();
-
+	private final Supplier<RamlTestCaseBuilder> builderSupplier;
+	
+	public RamlTestCasesVisitorFactory() {
+		this.builderSupplier = RamlTestCaseBuilder::new;
+	}
+	
+	private RamlTestCasesVisitorFactory(RamlTestCaseBuilder builder) {
+		this.builderSupplier = () -> new RamlTestCaseBuilder(builder.build());
+	}
+	
 	@Override
 	public ActionVisitor createActionVisitor() {
-		return new RamlTestCasesActionVisitor(builder, this);
+		RamlTestCaseBuilder builder = builderSupplier.get();
+		RamlVisitorFactory factory = new RamlTestCasesVisitorFactory(builder);
+		return new RamlTestCasesActionVisitor(builder, factory);
 	}
 
 	@Override
 	public RamlVisitor createRamlVisitor() {
-		return new RamlTestCasesVisitor(builder, this);
+		RamlTestCaseBuilder builder = builderSupplier.get();
+		RamlVisitorFactory factory = new RamlTestCasesVisitorFactory(builder);
+		return new RamlTestCasesVisitor(builder, factory);
 	}
 
 	@Override
 	public ResourceVisitor createResourceVisitor() {
-		return new RamlTestCasesResourceVisitor(builder, this);
+		RamlTestCaseBuilder builder = builderSupplier.get();
+		RamlVisitorFactory factory = new RamlTestCasesVisitorFactory(builder);
+		return new RamlTestCasesResourceVisitor(builder, factory);
 	}
-
+	
 	@Override
 	public ResponseVisitor createResponseVisitor() {
-		return new RamlTestCasesResponseVisitor(this.builder);
+		RamlTestCaseBuilder builder = builderSupplier.get();
+		return new RamlTestCasesResponseVisitor(builder);
 	}
 
 }
