c71c20670b013c0e110effdae9a4037b7ed59724
Claas Ahlrichs <claasahl@gmail.com>
2017-04-20 07:14:20 +0200

    organized imports

--- raml-junit-maven-plugin/src/test/java/com/github/claasahl/raml/visitor/CoordinatorTest.java
+++ raml-junit-maven-plugin/src/test/java/com/github/claasahl/raml/visitor/CoordinatorTest.java
@@ -1,18 +1,6 @@
 package com.github.claasahl.raml.visitor;
 
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-
-import java.util.Map;
-
 import org.junit.Test;
-import org.mockito.InOrder;
-import org.raml.model.MimeType;
-import org.raml.model.parameter.Header;
 
 public abstract class CoordinatorTest{
 
