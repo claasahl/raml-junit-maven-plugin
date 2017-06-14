54ae1f2e3e55480e8aa096edc35fd1652bff30f1
Claas Ahlrichs <claasahl@gmail.com>
2017-06-14 06:59:42 +0200

    revised and documented resource request

---
+++ raml-junit-maven-plugin/src/main/java/com/github/claasahl/raml/v08/junit/Parameter.java
@@ -0,0 +1,116 @@
+package com.github.claasahl.raml.v08.junit;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * The class {@link Parameter}.
+ * 
+ * @author Claas Ahlrichs
+ *
+ */
+public class Parameter {
+	private final String name;
+	private final List<String> values;
+
+	/**
+	 * Creates a new parameter.
+	 * 
+	 * @param name
+	 *            the parameter's name
+	 * @param values
+	 *            the parameter's value(s)
+	 */
+	public Parameter(String name, String... values) {
+		this.name = name;
+		if (values == null)
+			values = new String[0];
+		this.values = Collections.unmodifiableList(Arrays.asList(values));
+	}
+
+	/**
+	 * Creates a new parameter.
+	 * 
+	 * @param name
+	 *            the parameter's name
+	 * @param values
+	 *            the parameter's value(s)
+	 */
+	public Parameter(String name, List<String> values) {
+		this.name = name;
+		if (values == null)
+			values = new ArrayList<>();
+		this.values = Collections.unmodifiableList(new ArrayList<>(values));
+	}
+
+	/**
+	 * Returns the parameter's name.
+	 * 
+	 * @return the parameter's name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Returns the parameter's values. The returned list is immutable and
+	 * non-null. Typically, the list contains a single item (i.e. a direct
+	 * mapping between the parameter's name and it's value). The cases where the
+	 * parameter does not have a value or where the parameter has several values
+	 * are covered as well.
+	 * <p/>
+	 * <b>Examples:</b>
+	 * <ul>
+	 * <li>list contains 0 items: the parameter has no value (e.g.
+	 * <i>?parameter</i>)</li>
+	 * <li>list contains 1 item: the parameter has a single value (e.g.
+	 * <i>?parameter=value</i>)</li>
+	 * <li>list contains several items: the parameter has a multiple values
+	 * (e.g. <i>?parameter=value1&amp;parameter=value2</i>)</li>
+	 * </ul>
+	 * 
+	 * @return the parameter's values
+	 */
+	public List<String> getValues() {
+		return values;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((values == null) ? 0 : values.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Parameter other = (Parameter) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (values == null) {
+			if (other.values != null)
+				return false;
+		} else if (!values.equals(other.values))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Parameter [name=" + name + ", values=" + values + "]";
+	}
+
+}
--- raml-junit-maven-plugin/src/main/java/com/github/claasahl/raml/v08/junit/ResourceRequest.java
+++ raml-junit-maven-plugin/src/main/java/com/github/claasahl/raml/v08/junit/ResourceRequest.java
@@ -1,22 +1,84 @@
 package com.github.claasahl.raml.v08.junit;
 
-import java.util.Map;
+import java.util.Collection;
 
 public interface ResourceRequest {
-	Map<String, String> getQueryParameters();
+	// TODO implement sanity check for duplicate parameter names
+	// TODO implement sanity check for null-values in parameter collection
+	// TODO implement sanity check for multiple values (see path parameters)
+	
+	/**
+	 * Returns the <b>query</b> parameter names and their values to send with
+	 * the request. The parameter's name is expected to be unique within the
+	 * returned collection.
+	 * <p/>
+	 * <b>Note:</b> The returned collection may not be <code>null</code> nor may
+	 * it contain <code>null</code> values.
+	 * 
+	 * @return the query parameter names and their values to send with the
+	 *         request
+	 */
+	Collection<Parameter> getQueryParameters();
 
-	Map<String, String> getFormParameters();
+	/**
+	 * Returns the <b>form</b> parameter names and their values to send with the
+	 * request. The parameter's name is expected to be unique within the
+	 * returned collection.
+	 * <p/>
+	 * <b>Note:</b> The returned collection may not be <code>null</code> nor may
+	 * it contain <code>null</code> values.
+	 * 
+	 * @return the form parameter names and their values to send with the
+	 *         request
+	 */
+	Collection<Parameter> getFormParameters();
 
-	Map<String, String> getPathParameters();
+	/**
+	 * Returns the <b>path</b> parameter names and their values to send with the
+	 * request. The parameter's name is expected to be unique within the
+	 * returned collection. The parameters are expected to hold a single value
+	 * (i.e. exactly one value).
+	 * <p/>
+	 * <b>Note:</b> The returned collection may not be <code>null</code> nor may
+	 * it contain <code>null</code> values.
+	 * 
+	 * @return the path parameter names and their values to send with the
+	 *         request
+	 */
+	Collection<Parameter> getPathParameters();
 
-	Map<String, String> getHeaders();
+	/**
+	 * Returns the <b>header</b> names and their values to send with the
+	 * request. The header's name is expected to be unique within the returned
+	 * collection.
+	 * <p/>
+	 * <b>Note:</b> The returned collection may not be <code>null</code> nor may
+	 * it contain <code>null</code> values.
+	 * 
+	 * @return the header names and their values to send with the request
+	 */
+	Collection<Parameter> getHeaders();
 
-	Map<String, String> getCookies();
+	/**
+	 * Returns the <b>cookie</b> names and their values to send with the
+	 * request. The cookie's name is expected to be unique within the returned
+	 * collection.
+	 * <p/>
+	 * <b>Note:</b> The returned collection may not be <code>null</code> nor may
+	 * it contain <code>null</code> values.
+	 * 
+	 * @return the cookie names and their values to send with the request
+	 */
+	Collection<Parameter> getCookies();
 
 	String getVerb();
 
 	String getPath();
 
+	/**
+	 * Returns the content type to send with the request.
+	 * @return the content type to send with the request
+	 */
 	String getContentType();
 
 	String getBody();
