be1f6e916ca2c32d57accbe5ea2f137e79893ba3
Claas Ahlrichs <claasahl@gmail.com>
2017-07-04 09:16:04 +0200

    created model that identifies test cases

---
+++ raml-junit-maven-plugin/src/main/java/com/github/claasahl/raml/junit/api/TestCaseKey.java
@@ -0,0 +1,99 @@
+package com.github.claasahl.raml.junit.api;
+
+import javax.annotation.Nonnull;
+
+import com.github.claasahl.raml.junit.api.factories.RequestFactory;
+
+/**
+ * The class {@link TestCaseKey}.
+ * <p/>
+ * This class is meant to uniquely identify a (single) test case for a resource
+ * and RAML specification. As such, a test case can be identified for each
+ * combination of RAML specification (i.e. reference to the RAML document),
+ * request verb (i.e. HTTP verb of the HTTP request), request URL (i.e. URL of
+ * the HTTP request) and response code (i.e. status code of HTTP response).
+ * 
+ * @author Claas Ahlrichs
+ *
+ */
+public class TestCaseKey {
+	// TODO what is the "validation phase"? Define it in a document.
+
+	private final String ramlPath;
+	private final String requestVerb;
+	private final String requestUrl;
+	private final int responseCode;
+
+	/**
+	 * Creates a (unique) test case with the specified parameters.
+	 * 
+	 * @param ramlPath
+	 *            the path to the RAML specification
+	 * @param requestVerb
+	 *            the HTTP verb
+	 * @param requestUrl
+	 *            the (fully-qualified) URL
+	 * @param responseCode
+	 *            the status code
+	 */
+	public TestCaseKey(String ramlPath, String requestVerb, String requestUrl, int responseCode) {
+		this.ramlPath = ramlPath;
+		this.requestVerb = requestVerb;
+		this.requestUrl = requestUrl;
+		this.responseCode = responseCode;
+	}
+
+	/**
+	 * Returns the path to the RAML specification on which the test case is
+	 * base. This information will be used to retrieve constraints for both the
+	 * HTTP request and the HTTP response for this test case. Some
+	 * implementations of {@link RequestFactory} may also use this information
+	 * to generate a concrete HTTP request (e.g. by extracting example requests
+	 * or default values).
+	 * 
+	 * @return the path to the RAML specification on which the test case is base
+	 */
+	@Nonnull
+	public String getRamlPath() {
+		return ramlPath;
+	}
+
+	/**
+	 * Returns the (fully-qualified) URL of the test case.
+	 * <p/>
+	 * In the validation phase, the HTTP request will use this URL.
+	 * 
+	 * @return the URL of the test case
+	 */
+	@Nonnull
+	public String getRequestUrl() {
+		return requestUrl;
+	}
+
+	/**
+	 * Returns the HTTP verb of the test case. This may be any valid HTTP verb,
+	 * but most commonly one of <i>get</i>, <i>post</i>, <i>put</i> or
+	 * <i>delete</i>.
+	 * <p/>
+	 * In the validation phase, the HTTP request will use this verb.
+	 * 
+	 * @return the HTTP verb of the test case
+	 */
+	@Nonnull
+	public String getRequestVerb() {
+		return requestVerb;
+	}
+
+	/**
+	 * Returns the status code of the test case.
+	 * <p/>
+	 * In the validation phase, the HTTP response is expected to have this
+	 * status code.
+	 * 
+	 * @return the status code of the test case
+	 */
+	@Nonnull
+	public int getResponseCode() {
+		return responseCode;
+	}
+}
