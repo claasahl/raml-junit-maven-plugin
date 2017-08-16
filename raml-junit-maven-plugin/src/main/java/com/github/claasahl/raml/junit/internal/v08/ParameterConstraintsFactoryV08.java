3dddd5cc814e7d64938aef41ffa7443eccbd8633
Claas Ahlrichs <claasahl@gmail.com>
2017-08-16 07:14:12 +0200

    revised generation of parameter constraints (V08)

--- raml-junit-maven-plugin/src/main/java/com/github/claasahl/raml/junit/internal/v08/ParameterConstraintsFactoryV08.java
+++ raml-junit-maven-plugin/src/main/java/com/github/claasahl/raml/junit/internal/v08/ParameterConstraintsFactoryV08.java
@@ -1,18 +1,24 @@
 package com.github.claasahl.raml.junit.internal.v08;
 
+import static com.github.claasahl.raml.junit.internal.matchers.Matchers.everyValue;
 import static com.github.claasahl.raml.junit.internal.matchers.Matchers.hasLength;
-import static com.github.claasahl.raml.junit.internal.matchers.Matchers.*;
-import static org.hamcrest.Matchers.allOf;
+import static com.github.claasahl.raml.junit.internal.matchers.Matchers.hasName;
+import static com.github.claasahl.raml.junit.internal.matchers.Matchers.hasValues;
+import static com.github.claasahl.raml.junit.internal.matchers.Matchers.isInteger;
+import static com.github.claasahl.raml.junit.internal.matchers.Matchers.isRequired;
+import static com.github.claasahl.raml.junit.internal.matchers.Matchers.pattern;
+import static com.github.claasahl.raml.junit.internal.matchers.Matchers.toDouble;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.isIn;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.hamcrest.Description;
 import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.raml.v2.api.model.v08.parameters.IntegerTypeDeclaration;
 import org.raml.v2.api.model.v08.parameters.StringTypeDeclaration;
 
@@ -21,7 +27,8 @@ import com.github.claasahl.raml.junit.api.model.ParameterConstraints;
 
 public final class ParameterConstraintsFactoryV08 {
 
-	public static Collection<ParameterConstraints> createConstraints(Collection<org.raml.v2.api.model.v08.parameters.Parameter> parameters) {
+	public static Collection<ParameterConstraints> createConstraints(
+			Collection<org.raml.v2.api.model.v08.parameters.Parameter> parameters) {
 		Collection<ParameterConstraints> constraints = new ArrayList<>();
 		for (org.raml.v2.api.model.v08.parameters.Parameter parameter : parameters) {
 			constraints.add(createConstraints(parameter));
@@ -31,11 +38,11 @@ public final class ParameterConstraintsFactoryV08 {
 
 	private static ParameterConstraints createConstraints(org.raml.v2.api.model.v08.parameters.Parameter parameter) {
 		List<Matcher<Parameter>> matchers = new ArrayList<>();
-		if(Boolean.TRUE.equals(parameter.required())) {
+		if (Boolean.TRUE.equals(parameter.required())) {
 			matchers.add(isRequired());
 		}
 		matchers.add(hasName(equalTo(parameter.name())));
-		if(Boolean.TRUE.equals(parameter.repeat())) {
+		if (Boolean.TRUE.equals(parameter.repeat())) {
 			matchers.add(hasValues(hasSize(greaterThanOrEqualTo(1))));
 		}
 
@@ -49,61 +56,37 @@ public final class ParameterConstraintsFactoryV08 {
 		default:
 			System.err.println(parameter.type());
 		}
-		
+
 		return new ParameterConstraints(parameter.name(), matchers);
 	}
 
-	private static Matcher<String> createConstraints(final IntegerTypeDeclaration parameter) {
-		List<Matcher<? super String>> matchers = new ArrayList<>();
-		matchers.add(toDouble(lessThanOrEqualTo(2d)));
-		if (parameter.minimum() != null) {
-			matchers.add(toDouble(greaterThanOrEqualTo(parameter.minimum())));
-		}
-		if (parameter.maximum() != null) {
-			matchers.add(toDouble(lessThanOrEqualTo(parameter.maximum())));
-		}
-
-		return allOf(matchers);
-	}
-	
 	private static List<Matcher<Parameter>> createMatchers(IntegerTypeDeclaration parameter) {
 		List<Matcher<Parameter>> matchers = new ArrayList<>();
 		matchers.add(everyValue(isInteger()));
-		matchers.add(toDouble(lessThanOrEqualTo(2d)));
 		if (parameter.minimum() != null) {
-			matchers.add(toDouble(greaterThanOrEqualTo(parameter.minimum())));
+			matchers.add(everyValue(toDouble(greaterThanOrEqualTo(parameter.minimum()))));
 		}
-		everyItem(itemMatcher)
 		if (parameter.maximum() != null) {
-			matchers.add(toDouble(lessThanOrEqualTo(parameter.maximum())));
+			matchers.add(everyValue(toDouble(lessThanOrEqualTo(parameter.maximum()))));
 		}
-
 		return matchers;
 	}
 
-	private static Matcher<String> createConstraints(final StringTypeDeclaration parameter) {
-		List<Matcher<? super String>> matchers = new ArrayList<>();
+	private static List<Matcher<Parameter>> createMatchers(StringTypeDeclaration parameter) {
+		List<Matcher<Parameter>> matchers = new ArrayList<>();
 		if (parameter.pattern() != null) {
-			matchers.add(new TypeSafeMatcher<String>() {
-
-				@Override
-				public void describeTo(Description description) {
-					description.appendText("Does not match pattern: " + parameter.pattern());
-				}
-
-				@Override
-				protected boolean matchesSafely(String item) {
-					return item.matches(parameter.pattern());
-				}
-			});
+			matchers.add(everyValue(pattern(parameter.pattern())));
 		}
-		matchers.add(hasLength(lessThanOrEqualTo(2)));
+		if (parameter.enumValues() != null) {
+			List<String> enums = new ArrayList<>(parameter.enumValues());
+			matchers.add(everyValue(isIn(enums)));
+		}
 		if (parameter.minLength() != null) {
-			matchers.add(hasLength(greaterThanOrEqualTo(parameter.minLength())));
+			matchers.add(everyValue(hasLength(greaterThanOrEqualTo(parameter.minLength()))));
 		}
 		if (parameter.maxLength() != null) {
-			matchers.add(hasLength(lessThanOrEqualTo(parameter.maxLength())));
+			matchers.add(everyValue(hasLength(lessThanOrEqualTo(parameter.maxLength()))));
 		}
-		return allOf(matchers);
+		return matchers;
 	}
 }
