70d51cfce72544de6ea9aa76a26f079a14244319
Claas Ahlrichs <claasahl@gmail.com>
2017-09-11 06:59:58 +0200

    updated javadoc

--- raml-junit-maven-plugin/src/main/java/com/github/claasahl/raml/junit/internal/v08/package-info.java
+++ raml-junit-maven-plugin/src/main/java/com/github/claasahl/raml/junit/internal/v08/package-info.java
@@ -1,8 +1,31 @@
 /**
+ * Provides the means to map RAML V0.8 specifications to a common data model.
+ * This package contains implementations for the factories and related data
+ * models.
+ * <p/>
+ * A minimal implementation boils down to these factories and their referenced
+ * data models as listed below.
+ * <ul>
+ * <li>{@link com.github.claasahl.raml.junit.api.TestCaseFactory}</li>
+ * <ul>
+ * <li>{@link com.github.claasahl.raml.junit.api.TestCaseKey}</li>
+ * </ul>
+ * <li>{@link com.github.claasahl.raml.junit.api.RequestFactory}</li>
+ * <ul>
+ * <li>{@link com.github.claasahl.raml.junit.api.model.Request}</li>
+ * <li>{@link com.github.claasahl.raml.junit.api.model.Parameter}</li>
+ * <li>{@link com.github.claasahl.raml.junit.api.model.Body}</li>
+ * </ul>
+ * <li>{@link com.github.claasahl.raml.junit.api.ConstraintsFactory}</li>
+ * <ul>
+ * <li>{@link com.github.claasahl.raml.junit.api.model.RequestConstraints}</li>
+ * <li>{@link com.github.claasahl.raml.junit.api.model.ResponseConstraints}</li>
+ * <li>{@link com.github.claasahl.raml.junit.api.model.ParameterConstraints}</li>
+ * <li>{@link com.github.claasahl.raml.junit.api.model.BodyConstraints}</li>
+ * </ul>
+ * </ul>
  * 
- */
-/**
- * @author Claas
+ * @author Claas Ahlrichs
  *
  */
 package com.github.claasahl.raml.junit.internal.v08;
