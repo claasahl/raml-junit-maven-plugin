b5f56eeff1ff1ac582d3d3c37f355ebb70c0c5bb
Claas Ahlrichs <claasahl@gmail.com>
2017-10-01 19:28:29 +0200

    revised conversion of parameters to rest assured

--- raml-junit-maven-plugin/src/main/java/com/github/claasahl/raml/junit/internal/common/RestAssuredResponseSupplier.java
+++ raml-junit-maven-plugin/src/main/java/com/github/claasahl/raml/junit/internal/common/RestAssuredResponseSupplier.java
@@ -34,27 +34,52 @@ public class RestAssuredResponseSupplier implements ResponseSupplier {
 		return null;
 	}
 
+	/**
+	 * A utility function that converts {@link Parameter}s (internal to this
+	 * framework) into parameters usable by rest assured.
+	 * 
+	 * @param parameters
+	 *            the parameters
+	 * @return
+	 */
 	private static Map<String, ?> parameters(Collection<Parameter> parameters) {
-		return parameters.stream().collect(Collectors.toMap(Parameter::getName, Parameter::getValues));
+		return parameters.stream().collect(Collectors.toMap(Parameter::getName, RestAssuredResponseSupplier::values));
 	}
 
+	private static Object values(Parameter parameter) {
+		if (parameter == null || parameter.getValues().isEmpty()) {
+			return null;
+		} else if (parameter.getValues().size() == 1) {
+			return parameter.getValues().get(0);
+		} else {
+			return parameter.getValues();
+		}
+	}
+
+	/**
+	 * A utility function that performs an HTTP request and
+	 * 
+	 * @param request
+	 * @return
+	 */
 	private static Response send(Request request) {
 		try {
 			String method = request.getRequestVerb();
-			String path = request.getRequestUrl();
+			String path = "http://xkcd.com" + request.getRequestUrl();
 			Body body = request.getRequestBody();
-			RequestSpecification specification = given().queryParams(parameters(request.getRequestQueryParameters()))
+			RequestSpecification specification = given().log().all()
+					.queryParams(parameters(request.getRequestQueryParameters()))
 					.formParams(parameters(request.getRequestFormParameters()))
 					.pathParams(parameters(request.getRequestPathParameters()))
 					.headers(parameters(request.getRequestHeaders())).cookies(parameters(request.getRequestCookies()));
 			if (body != null) {
 				specification = specification.contentType(body.getContentType()).body(body.getContent());
 			}
-			return specification.when().request(method, path);
+			return specification.then().log().all().when().request(method, path);
 		} catch (Exception e) {
 			// TODO log error message
 		}
 		return null;
-	}	
+	}
 
 }
